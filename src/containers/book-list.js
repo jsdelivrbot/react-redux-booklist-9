d5ecf4b380815c9ebc2f10aa005c0b93abfc3b01
vince preziose <vince@softphusion.com>
2018-01-28 00:40:57 -0800

    create an action

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -8,7 +8,12 @@ class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-              <li key={ book.title } className="list-group-item">{ book.title }</li>
+              <li
+                  key={ book.title }
+                  onClick={ () => this.props.selectBook(book) }
+                  className="list-group-item">
+                  { book.title }
+              </li>
             );
         });
     }
@@ -38,4 +43,4 @@ function mapDispatchToProps(dispatch) {
 
 // Promote Booklist from a component to a container - it needs to know about this new dispatch
 // method, selectBook. Make it available as a prop.
-export default connect(mapStateToProps, mapDispatchToProps())(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
